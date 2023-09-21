6194dbb5073bc3f38442bab195c281c037c571c6
Tan Nicholas <tannicholas54@gmail.com>
2023-09-21 22:55:46 +0700

    betulin database user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('alamatUser');
-            $table->string('no_User');
-            $table->string('foto_user');
+            $table->string('alamatUser')->nullable();
+            $table->string('no_User')->nullable();
+            $table->string('foto_user')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
